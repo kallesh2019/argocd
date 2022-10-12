1c067f1a27ec1b744ba417db4fc5ba0174f0b79f
kallesh2019 <46673053+kallesh2019@users.noreply.github.com>
2022-10-12 16:59:40 +0530

    Demo app updated

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'This APP Deployed Via ArgoCD 12-10-2022 Thank you!!!'
+    return 'This  is DEMO  APP Deployed Via ArgoCD 12-10-2022 Thank you!!!'
